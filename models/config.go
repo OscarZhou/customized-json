021a47a4cc373bd5667cec9447d99f59423b59d0
OscarZhou <zhouhongyu1989@gmail.com>
2018-05-30 18:34:42 +1200

    Add scanAPIFiles

--- models/config.go
+++ models/config.go
@@ -1,4 +1,24 @@
 package models
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
+	Templates []ServerTemplate
+	FileList  []string
+}
+
+func (c *Config) scanAPIFiles(path string) error {
+	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		c.FileList = append(c.FileList, path)
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
