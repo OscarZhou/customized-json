02b41f5438955c57f8c90f8546a195169ac71261
OscarZhou <zhouhongyu1989@gmail.com>
2018-06-15 12:21:14 +1200

    Update the initialisation of the template

--- main.go
+++ main.go
@@ -4,44 +4,64 @@ import (
 	"customized-json/models"
 )
 
-func main() {
-	var templates []models.Template
+var templates []models.Template
+
+func init() {
 
 	t := models.Template{
-		ControllerPath: `C:\Users\project\ems\controllers`,
 		ServerTemplate: models.ServerTemplate{
-			ProjectVersion: "v0.4",
+			ProjectVersion: "v1",
 			APIVersion:     "v1",
 			ProxySchema:    "http",
-			ProxyPass:      "127.0.0.1:9100",
-			CustomConfigs: models.CustomConfig{
-				"Cached":                  true,
-				"CachedDurationsInSecond": 10,
-				"Authentication":          true,
-			},
-		},
-		APIs: make(map[string][]string),
-	}
-	templates = append(templates, t)
-
-	t = models.Template{}
-	t = models.Template{
-		ControllerPath: `C:\Users\project\cms\controllers`,
-		ServerTemplate: models.ServerTemplate{
-			ProjectVersion: "v0.4",
-			APIVersion:     "v1",
-			ProxySchema:    "http",
-			ProxyPass:      "127.0.0.1:9101",
+			ProxyPass:      "127.0.0.1:8021",
 			CustomConfigs: models.CustomConfig{
 				"Cached":                  true,
 				"CachedDurationsInSecond": 10,
 				"Authentication":          false,
 			},
 		},
-		APIs: make(map[string][]string),
+		APIs:      make(map[string][]string),
+		Resources: []string{},
 	}
 	templates = append(templates, t)
 
+	t = models.Template{
+		ServerTemplate: models.ServerTemplate{
+			ProjectVersion: "v0.1",
+			APIVersion:     "v1",
+			ProxySchema:    "http",
+			ProxyPass:      "127.0.0.1:8021",
+			CustomConfigs: models.CustomConfig{
+				"Cached":                  true,
+				"CachedDurationsInSecond": 10,
+				"Authentication":          true,
+			},
+		},
+		APIs:      make(map[string][]string),
+		Resources: []string{},
+	}
+	templates = append(templates, t)
+
+	t = models.Template{
+		ServerTemplate: models.ServerTemplate{
+			ProjectVersion: "",
+			APIVersion:     "v1",
+			ProxySchema:    "http",
+			ProxyPass:      "127.0.0.1:8021",
+			CustomConfigs: models.CustomConfig{
+				"Cached":                  true,
+				"CachedDurationsInSecond": 10,
+				"Authentication":          false,
+			},
+		},
+		APIs:      make(map[string][]string),
+		Resources: []string{},
+	}
+	templates = append(templates, t)
+
+}
+
+func main() {
 	c := models.NewConfig(templates)
 	err := c.OutputConfigFile("route")
 	if err != nil {
